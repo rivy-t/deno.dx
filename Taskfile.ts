45a488d2608a9aab74965417687abe1c7889639b
Roy Ivy III <rivy.dev@gmail.com>
2024-07-07 00:12:24 -0500

    refactor ~ replace 'denopkg.com' imports with JSDelivr CDN (more established/stable)

--- Taskfile.ts
+++ Taskfile.ts
@@ -1,5 +1,5 @@
 /// <reference lib="esnext" />
-import {$, $o, $a, cd, pwd, question, glob, echo, cat,sleep,printf} from 'https://denopkg.com/linux-china/dx/mod.ts';
+import {$, $o, $a, cd, pwd, question, glob, echo, cat,sleep,printf} from 'https://cdn.jsdelivr.net/gh/linux-china/deno.dx@206051e/mod.ts';
 import {red, yellow, blue, green} from "https://deno.land/std@0.105.0/fmt/colors.ts";
 
 export default hello;
